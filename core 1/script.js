66d27d86e3866ba845a683e2fcc9b5dfcbd2689a
hyop24 <huans735@newschool.edu>
2024-11-19 09:35:48 -0500

    Delete core 1/script.js

--- core 1/script.js
+++
@@ -1,9 +0,0 @@
-var button = document.querySelector ('button');
-var body = document.body;
-
-button.onclick = function changeColor() {
-	if (body.style.backgroundColor == 'white') 
-	{body.style.backgroundColor = 'pink';}
-	 else {
-	body.style.backgroundColor = 'white';}
-};
